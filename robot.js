e59b8fe37f09079405c14bcb577f15622c1407ae
pierre@teilhard.name <pierre@teilhard.name>
2015-10-19 15:32:05 +0200

    charte bonne conduite respectee

--- robot.js
+++ robot.js
@@ -3,21 +3,19 @@
  */
 
 var page = require('webpage').create();
-console.log('The default user agent is ' + page.settings.userAgent);
-page.settings.userAgent = 'SpecialAgent';
 page.open('http://www.easyvirt.com/nous-contacter/', function(status) {
     if (status !== 'success') {
     } else {
         var mailTos = page.evaluate(function() {
             var a = [].slice.call(document.querySelectorAll("a"));
-            var b = [];
+            var aretourner = [];
             for (var i=0; i < a.length; i++){
-                var c = a[i].getAttribute("href");
-                if (c!=undefined && c.search("mailto:")!=-1) {
-                    b.push(c.substring(7));
+                var href = a[i].getAttribute("href");
+                if (href!=undefined && href.search("mailto:")!=-1) {
+                    aretourner.push(href.substring(7));
                 }
             }
-            return b;
+            return aretourner;
             });
         for (var i=0; i < mailTos.length; i++) {
             console.log(mailTos[i]);
