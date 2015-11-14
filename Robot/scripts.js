7a69fa5aa7553c728152cb1777b9cad06b7679dc
pierre@teilhard.name <pierre@teilhard.name>
2015-11-14 17:01:44 +0100

    add scripts, working fine

--- Robot/scripts.js
+++ Robot/scripts.js
@@ -1,18 +1,18 @@
 /**
- * Created by Pierre on 10/11/2015.
+ * Created by Pierre on 14/11/2015.
  */
+
 var cheerio = require("cheerio");
 var request = require("request");
 var fs = require('fs');
 
-request('https://web.emn.fr/x-info/miel/PotDeMiel.php', function (error, response, html) {
+request('https://web.emn.fr/x-info/miel/css/style.css', function (error, response, html) {
     if (!error && response.statusCode == 200) {
         var $ = cheerio.load(html);
         var a = $.html();
-        console.log(a);
-        fs.writeFile("D:\\XAMPP\\htdocs\\projetoption\\a.html",a);
+        fs.writeFile("D:\\XAMPP\\htdocs\\projetoption\\css\\style.css",a);
         console.log('done');
-        return a;
+        //return a;
     }
     else {
         console.log("echec");
